d039bcdfbbcb63c6d7178c14d1d9d390acc1bf9e
Aya <ayafikry33@gmail.com>
2020-05-01 11:57:15 +0200

    Home Structure

--- Assets/js/BookStoreScript.js
+++ Assets/js/BookStoreScript.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){ 
+    var addbtn = document.getElementById("add");
+    var table = document.getElementsByTagName("table")[0]
+    addbtn.addEventListener("click", function(){
+        table.innerHTML += `<tr>
+                            <td><input type="checkbox"></td>
+                            <td><input type="text"></td>
+                            <td><input type="text"></td>
+                            <td><input type="text"></td>
+                            <td><input type="text"></td>
+                            <td><input type="text"></td>
+                        </tr>`
+    })
+}); 
+
+var a = document.querySelectorAll("nav .navbar-nav .nav-item a")
+a.forEach(element => {
+    element.addEventListener("click", function(e){
+        var x = e.target.getAttribute("link")
+        document.getElementById(x).style.display = "block"
     
+    })
 }); 
